971632466691c1aec5d2696906a59ddd8d1368db
Charles Oliver <cpoliver@gmail.com>
2016-09-03 15:04:45 +0100

    stripped out scaffolding. added demo component

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>&lt;app goes here /&gt;</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
