38d41e1fa9e02ec828700687a2605841b5b58920
Charles Oliver <cpoliver@gmail.com>
2016-09-05 01:39:13 +0100

    added thunk and logger middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+
+const loggerMiddleware = createLogger();
+
 import rootReducer from './reducers';
 
 export function makeStore() {
-  return createStore(rootReducer);
+  return createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
+  );
 }
