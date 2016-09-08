42a7bb14db5212d64b34284850122a25cfc68aa7
Charles Oliver <cpoliver@gmail.com>
2016-09-08 08:49:49 +0100

    adding tests for feed data action creator

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,16 @@
 import { fromJS } from 'immutable';
 
+// Helpers
 export const INIT_STATE = fromJS({
   feeds: {}
 });
 
 export const NO_OP = () => {};
+
+// Feed Config
+const NUMBER_OF_ENTRIES = 3;
+
+const CORS_PROXY_URL = 'https://crossorigin.me';
+const FEED_API_URL = `https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=${NUMBER_OF_ENTRIES}&q=`;
+
+export const buildUrl = feedUrl => `${CORS_PROXY_URL}/${FEED_API_URL}${feedUrl}`;
